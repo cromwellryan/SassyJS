ad4321c378c15782309a2d949fc89c961cff9ee2
Ryan Cromwell <cromwellryan@hotmail.com>
2011-06-08 14:33:14 -0400

    initial commit of sassy.js

---
+++ example/server.js
@@ -0,0 +1,18 @@
+var http = require('http')
+	, url = require('url')
+	, respond = require('../')
+	, server;
+
+server = http.createServer(function(req,res) {
+	var path = url.parse(req.url).pathname;
+
+	switch(path) {
+		case "/":
+			respond.to(res).with.html.content("hello there!");
+			break;
+		default: respond.to(res).with.html.send404();
+	}
+}),
+
+server.listen(8080);
+
